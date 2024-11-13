9b92da281271913347581a5fb116ec0a5cdea97b
daniel-lujan <daniel.lujan@udea.edu.co>
2024-11-12 19:28:58 -0500

    fix: :bug: Fix database connection dependency function

--- app/database/__init__.py
+++ app/database/__init__.py
@@ -6,14 +6,8 @@ shops_db = ShopsClient()
 
 
 async def get_client_db():
-    try:
-        yield client_db
-    finally:
-        await client_db.disconnect()
+    yield client_db
 
 
 async def get_shops_db():
-    try:
-        yield shops_db
-    finally:
-        await shops_db.disconnect()
+    yield shops_db
