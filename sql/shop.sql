8ffd57f07d2e8d365da6286a56b1f3e57877909a
daniel-lujan <daniel.lujan@udea.edu.co>
2024-12-02 11:03:49 -0500

    feat: :card_file_box: Add quantity column for order-items relation

--- sql/shop.sql
+++ sql/shop.sql
@@ -94,6 +94,7 @@ CREATE TABLE
         id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
         order_id BIGINT NOT NULL,
         product_id BIGINT NOT NULL,
+        quantity INT NOT NULL,
         FOREIGN KEY (order_id) REFERENCES orders (id),
         FOREIGN KEY (product_id) REFERENCES products (id)
     );
