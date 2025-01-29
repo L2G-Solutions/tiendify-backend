f1cc461333ca92b4a1e10dd8fd12f5bb60b38082
daniel-lujan <daniel.lujan@udea.edu.co>
2025-01-29 17:48:53 -0500

    refactor: :recycle: Re-order cloud resources creation

--- app/core/cloud_provisioning/__init__.py
+++ app/core/cloud_provisioning/__init__.py
@@ -10,8 +10,8 @@ from app.database import client_db as db
 from app.services.azure.provisioning.database import create_postgresql_database
 from app.services.azure.provisioning.storage import create_public_container
 from app.services.azure.provisioning.webapp import create_web_app
-from database.client_db.models import shop
 from app.services.keycloak.realm import create_keycloak_realm
+from database.client_db.models import shop
 
 
 def get_database_resource_name(shop_id: str) -> str:
@@ -52,20 +52,19 @@ async def create_cloud_resources_for_user(shop_id: str) -> shop:
         {"resource_group_id": rg.id}, {"id": shop_id}, include={"users": True}
     )
 
-    database = create_postgresql_database(
-        get_database_resource_name(shop_id),
-        admin_user=settings.AZURE_DB_DEFAULT_USERNAME,
-        admin_password=settings.AZURE_DB_DEFAULT_PASSWORD,
-    )
-    
     keycloak_info = await create_keycloak_realm(shop_id)
     realm_name = keycloak_info["realm_name"]
     client_id = keycloak_info["client_id"]
     client_secret = keycloak_info["client_secret"]
 
     await db.resource_group.update(
-        where={"id": rg.id},
-        data={"keycloak_realm": realm_name}
+        where={"id": rg.id}, data={"keycloak_realm": realm_name}
+    )
+
+    database = create_postgresql_database(
+        get_database_resource_name(shop_id),
+        admin_user=settings.AZURE_DB_DEFAULT_USERNAME,
+        admin_password=settings.AZURE_DB_DEFAULT_PASSWORD,
     )
 
     await db.resource_group.update(
@@ -85,7 +84,7 @@ async def create_cloud_resources_for_user(shop_id: str) -> shop:
     backend = create_web_app(
         get_asp_resource_name(shop_id),
         get_web_app_resource_name(shop_id),
-        env_vars = {
+        env_vars={
             "PROJECT_NAME": updated_shop.name,
             "DATABASE_URL": build_database_url(
                 database.fully_qualified_domain_name,
@@ -94,12 +93,12 @@ async def create_cloud_resources_for_user(shop_id: str) -> shop:
             ),
             "AZURE_STORAGE": settings.AZURE_DEFAULT_STORAGE_ACCOUNT,
             "AZURE_PUBLIC_CONTAINER": storage.container_name,
-            "SECRET_KEY": settings.STORE_API_SCRET_KEY,
+            "SECRET_KEY": settings.STORE_API_SECRET_KEY,
             "KEYCLOAK_URL": settings.KEYCLOAK_URL,
-            "KEYCLOAK_CLIENT_ID": client_id, 
+            "KEYCLOAK_CLIENT_ID": client_id,
             "KEYCLOAK_REALM": realm_name,
             "KEYCLOAK_CLIENT_SECRET": client_secret,
-        }
+        },
     )
 
     await db.resource_group.update(
@@ -110,8 +109,8 @@ async def create_cloud_resources_for_user(shop_id: str) -> shop:
         where={"id": rg.id},
     )
 
-    if len(updated_shop.users) > 0:
-        user = updated_shop.users[0]
+    if updated_shop.users is not None:
+        user = updated_shop.users
         if user:
             send_store_created_email(user.first_name, user.email, updated_shop.name)
 
